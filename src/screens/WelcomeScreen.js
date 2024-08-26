7d1a23e882651a5aa85ba8964b8cc69b6d12bf5f
Fouad-berry <fidriss96@gmail.com>
2024-08-26 19:29:30 +0200

    add react reanimated package and update welcome screen style

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,19 +1,40 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import tw from 'twrnc'
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import tw from 'twrnc';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default function WelcomeScreen() {
   return (
-    <View className=""
-    style={tw`flex-1 justify-center items-center space-y-10 bg-amber-500`}>
+    <View
+      style={tw`flex-1 justify-center items-center bg-amber-500`}
+    >
       <StatusBar style="light" />
-      <View  style={tw`bg-white/20 rounded-full`} >
-        <View style={tw`bag-white/20 rounded-ful`}>
-            <Image source={require('../../assets/images/foodyhd.png')}
-                style={{width:200, height:200}} />
+      <View
+        style={[tw`bg-white/20 rounded-full mb-10`, { padding: hp(5.5) }]}
+      >
+        <View
+          style={[tw`bg-white/20 rounded-full`, { padding: hp(5) }]}
+        >
+          <Image
+            source={require('../../assets/images/foodyhd.png')}
+            style={{ width: hp(20), height: hp(20) }}
+          />
         </View>
       </View>
+
+      <View style={tw`flex items-center`}>
+        <Text
+          style={[tw`font-bold text-white mb-2`, { fontSize: hp(7), letterSpacing: hp(0.1) }]}
+        >
+          Foody
+        </Text>
+        <Text
+          style={[tw`font-medium text-white`, { fontSize: hp(2), letterSpacing: hp(0.05) }]}
+        >
+          Food, recipe for us
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
