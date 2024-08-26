db6213529bd39f9ea9f02d0eb506987a51d5a17e
Fouad-berry <fidriss96@gmail.com>
2024-08-26 21:13:05 +0200

    update Homescreen logic

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, ScrollView, Image } from 'react-native'
 import React from 'react'
+import tw from 'twrnc'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import {BellIcon} from 'react-native-heroicons/outline'
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    return (
+        <View style={tw`flex-1 bg-white`}>
+            <StatusBar style="dark" />
+            <ScrollView
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 50}}
+                style={tw`pt-8 mb-6`}
+            >
+                <View style={tw`mx-4 flex-row justify-between items-center mb-4`}>
+                    <Image source={require('../../assets/images/avatar.png')} style={{height: hp(5), width: hp(5.5)}}/>
+                    <BellIcon size={hp(4)} color="gray" />
+                </View>
+
+                <View style={tw`mx-4 space-y-2 mb-2`}>
+                    <Text style={[tw`text-neutral-600`, { fontSize: hp(1.7)}]}>Bonjour, Fouad</Text>
+                </View>
+            </ScrollView>
+        </View>
+    )
 }
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
 import { View, Text, Image } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import tw from 'twrnc';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native';
 
 export default function WelcomeScreen() {
+
+    const ring1padding = useSharedValue(0);
+    const ring2padding = useSharedValue(0);
+
+    const navigation = useNavigation();
+
+    useEffect(()=>{
+        ring1padding.value = 0;
+        ring2padding.value = 0;
+        setTimeout(()=> ring1padding.value = (ring1padding.value+hp(5)), 100);
+        setTimeout(()=> ring2padding.value = (ring2padding.value+hp(5.5)), 300);
+
+        setTimeout(()=> navigation.navigate('Home'), 2500)
+    },[])
+
   return (
     <View
       style={tw`flex-1 justify-center items-center bg-amber-500`}
