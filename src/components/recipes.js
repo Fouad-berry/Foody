71b513aabea4ebb4d94c2cc389ea18a5d9ba526f
Fouad-berry <fidriss96@gmail.com>
2024-08-28 15:59:22 +0200

    update recipe logice to fetch mealdata

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -1,10 +1,34 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import tw from 'twrnc'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import MasonryList from '@react-native-seoul/masonry-list';
+import { mealData } from '../constants';
 
 export default function Recipes() {
   return (
-    <View>
-      <Text>recipes</Text>
+    <View style={tw`mb-3 mx-4`}>
+        <Text style={[tw`font-semibold text-neutral-600`, { fontSize: hp(3)}]}>Recettes</Text>
+        <View>
+            <MasonryList
+                data={mealData}
+                keyExtractor={(item) => item.id}
+                numColumns={2}
+                showsVerticalScrollIndicator={false}
+                renderItem={({item, i}) => <RecipeCard  item={item} index={i}/>}
+//                onRefresh={() => refetch({first: ITEM_CNT})}
+                onEndReachedThreshold={0.1}
+//                onEndReached={() => loadNext(ITEM_CNT)}
+            />
+        </View>
     </View>
   )
+}
+
+const RecipeCard = ({item, index})=>{
+    return(
+        <View>
+            <Text>Recipe</Text>
+        </View>
+    )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,3 +12,26 @@ export const categoryData =[
         image: "https://www.themealdb.com/images/category/dessert.png"
     }
 ]
+
+export const mealData =[
+    {
+        name: 'Shakshuka',
+        image: "https://www.themealdb.com/images/category/g373701551450225.png"
+    },
+    {
+        name: 'bol de banh mi au bœuf avec mayonnaise à la sriracha',
+        image: "https://www.themealdb.com/images/media/meals/z0ageb1583189517.jpg"
+    },
+    {
+        name: 'Chickpeas Fajitas',
+        image: "https://www.themealdb.com/images/media/meals/tvtxpq1511464705.jpg"
+    },
+    {
+        name: 'chili aux lentilles fumées et à la courge',
+        image: "https://www.themealdb.com/images/media/meals/uwxqwy1483389553.jpg"
+    },
+    {
+        name: 'Boeuf braisé Chili',
+        image: "https://www.themealdb.com/images/media/meals/uuqvwu1504629254.jpg"
+    }
+]
