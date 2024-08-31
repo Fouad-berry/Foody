efd00bc50a2d81cbe51e6335e8965bed67c5df15
Fouad-berry <fidriss96@gmail.com>
2024-08-31 09:22:33 +0200

    add Recipe detailScreen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
+import RecipeDetailScreen from '../screens/RecipeDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ function AppNavigation() {
         <Stack.Navigator initialRouteName='Welcome' screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
         </Stack.Navigator>
     );
 }
