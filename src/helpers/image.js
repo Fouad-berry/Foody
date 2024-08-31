bec74bff928c2f09cc303dd0af51a6ba2de8acfb
Fouad-berry <fidriss96@gmail.com>
2024-08-31 09:10:50 +0200

    update component image logic on helpers

--- src/helpers/image.js
+++ src/helpers/image.js
@@ -1,10 +1,38 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function image() {
-  return (
-    <View>
-      <Text>image</Text>
-    </View>
-  )
+import Animated from 'react-native-reanimated'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const CachedImage = (props)=> {
+    const [cachedSource, setCachedSource] = useState(null);
+    const {uri} = props;
+
+    useEffect(() => {
+        const getCachedImage = async () => {
+            try{
+                const CachedImageData = await AsyncStorage.getItem(uri);
+                if (CachedImageData) {
+                    setCachedSource({ uri: CachedImageData});
+                }else {
+                    const response = await fetch(uri);
+                    const imageBlob = await response.blob();
+                    const base64Data = await new Promise((resolve) => {
+                        const reader = new FileReader();
+                        reader.readAsDataURL(imageBlob);
+                        reader.onloadend = () => {
+                            resolve(reader.result);
+                        };
+                    });
+                    await AsyncStorage.setItem(uri, base64Data);
+                    setCachedSource({ uri: base64Data});
+                }
+            } catch (error) {
+                console.log('Error cachin image',error);
+                setCachedSource({uri});
+            }
+        };
+        getCachedImage();
+    }, []);
+  return <Animated.Image source={cachedSource} {...props} />
 }
