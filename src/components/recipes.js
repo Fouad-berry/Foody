67755a7719df9cbe300d96d94d805f81313d2368
Fouad-berry <fidriss96@gmail.com>
2024-08-31 10:51:42 +0200

    update recipe logic

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -7,9 +7,11 @@ import { mealData } from '../constants';
 import Animated, {FadeInDown} from 'react-native-reanimated';
 import Loading from './loading';
 import { CachedImage } from '../helpers/image';
+import { useNavigation } from '@react-navigation/native';
 
 
 export default function Recipes({categories, meals}) {
+    const navigation = useNavigation();
   return (
     <View style={tw`mb-3 mx-4`}>
         <Text style={[tw`font-semibold text-neutral-600`, { fontSize: hp(3)}]}>Recettes</Text>
@@ -23,7 +25,7 @@ export default function Recipes({categories, meals}) {
                     keyExtractor={(item) => item.idMeal}
                     numColumns={2}
                     showsVerticalScrollIndicator={false}
-                    renderItem={({item, i}) => <RecipeCard  item={item} index={i}/>}
+                    renderItem={({item, i}) => <RecipeCard  item={item} index={i} navigation={navigation} />}
     //                onRefresh={() => refetch({first: ITEM_CNT})}
                     onEndReachedThreshold={0.1}
     //                onEndReached={() => loadNext(ITEM_CNT)}
@@ -35,12 +37,13 @@ export default function Recipes({categories, meals}) {
   )
 }
 
-const RecipeCard = ({item, index})=>{
+const RecipeCard = ({item, index, navigation})=>{
     let isEven = index%2==0;
     return(
         <Animated.View entering={FadeInDown.delay(index*100).duration(600).springify().damping(12)}>
             <Pressable
                 style={[tw`flex justify-center mb-4 space-y-1`, { width: '100%', paddingLeft: isEven? 0:8, paddingRight: isEven?8:0}]}
+                onPress={()=> navigation.navigate('RecipeDetail', {...item})}
             >
 {/*                 <Image
                     source={{uri: item.strMealThumb}}
