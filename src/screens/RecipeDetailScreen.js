a6f110c04c7b2c45bad04531ace5343cf0117929
Fouad-berry <fidriss96@gmail.com>
2024-09-01 18:16:10 +0200

    update recipedetailscreen

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -1,13 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, StatusBar } from 'react-native'
 import React from 'react'
+import tw from 'twrnc';
+import { CachedImage } from '../helpers/image';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
 
 export default function RecipeDetailScreen(props) {
-  let item = props.route.param;
+  let item = props.route.params;
 
-  
+
   return (
-    <View>
-      <Text>RecipeDetailScreen</Text>
-    </View>
+    <ScrollView
+      style={tw`bg-white flex-1`}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{paddingBottom: 30}}
+    >
+      <StatusBar style={"light"} />
+      <View style={tw`flex-row justify-center`}>
+        <CachedImage
+            uri={item.strMealThumb}
+            style={{width: wp(98), height: hp(50), borderRadius: 30, borderBottomLeftRadius: 30, borderBottomRightRadius: 30, marginTop: 4}}
+        />
+      </View>
+    </ScrollView>
   )
 }
