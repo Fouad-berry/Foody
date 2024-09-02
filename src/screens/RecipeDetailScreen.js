c46cbc908fe4df7440836febd92cc61171112b9f
Fouad-berry <fidriss96@gmail.com>
2024-09-02 19:49:18 +0200

    add Loading component to Recipedetailscreen

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -7,13 +7,14 @@ import { ChevronLeftIcon } from 'react-native-heroicons/outline';
 import {  HeartIcon } from 'react-native-heroicons/solid';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
+import Loading from '../components/loading';
 
 
 export default function RecipeDetailScreen(props) {
   let item = props.route.params;
   const [isFavourite, setIsFavourite] = useState(false)
   const navigation = useNavigation();
-  
+  const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
     getMealData(item.idMeal);
@@ -22,11 +23,12 @@ export default function RecipeDetailScreen(props) {
   const getMealData = async (id)=>{
     try{
         const response = await axios.get(`https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-        console.log('got recipes:', response.data);
-/*         if(response && response.data){
-            setMeals(response.data.meals);
+        console.log('got meal data:', response.data);
+          if(response && response.data){
+            setMeal(response.data.meals[0]);
+            setLoading(false);
         }
- */    }catch(err){
+      }catch(err){
         console.log('errors:', err.message);
     }
 }
@@ -54,6 +56,16 @@ export default function RecipeDetailScreen(props) {
           <HeartIcon size={hp(3.5)} strokeWidth={4.5} color={isFavourite? "red": "gray"} />
         </TouchableOpacity>
       </View>
+
+      {
+        loading? (
+          <Loading size="large" style={tw`mt-16`} />
+        ):(
+          <View>
+            <Text>display meal data</Text>
+          </View>
+        )
+      }
     </ScrollView>
   )
 }
